0900a4504e9fe734513d4905037c2c567d8ecc0d
Thainá Pires <thainaspiress@gmail.com>
2021-07-21 20:23:34 -0300

    Adding frontend

---
+++ aluraflix-front/build/precache-manifest.1f7fe32610a555ab912516abb7ebe3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc8e532a99e0f85751c12ef3734f5da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002c40da0e106bf81fbf",
+    "url": "/static/css/main.43b442d7.chunk.css"
+  },
+  {
+    "revision": "b62fb2f4c9d2c74d2508",
+    "url": "/static/js/2.da6461de.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.da6461de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002c40da0e106bf81fbf",
+    "url": "/static/js/main.ff2b60e0.chunk.js"
+  },
+  {
+    "revision": "d4a5648972102e88a2a2",
+    "url": "/static/js/runtime-main.e1ab3c87.js"
+  },
+  {
+    "revision": "b1f5d75a009bbda15bd3ab76ed17cc50",
+    "url": "/static/media/Logo.b1f5d75a.png"
+  }
+]);
